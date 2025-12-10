be1c83fdc3a7cf13fa442c565b81974c6798ddb0
fadcon <fad.con0909@gmail.com>
2025-12-11 00:32:59 +0400

    fix CodeStyle

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
@@ -2,13 +2,11 @@ package ru.yandex.practicum.sleeptracker;
 
 import ru.yandex.practicum.sleeptracker.io.IOFileReaderService;
 import ru.yandex.practicum.sleeptracker.functions.*;
-import ru.yandex.practicum.sleeptracker.model.Chronotype;
 import ru.yandex.practicum.sleeptracker.model.SleepAnalysisResult;
 import ru.yandex.practicum.sleeptracker.model.SleepSession;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
